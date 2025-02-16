12fbb9db7f40f2c62d59db4f59f02eeb6ab76021
grant khariton widjaja <grantkhariton@gmail.com>
2025-02-16 16:16:59 +0100

    test

---
+++ pa-ws2425/main.py
@@ -0,0 +1,14 @@
+import datetime
+import os
+
+import numpy as np
+
+import project.functions as fn
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ pa-ws2425/project/functions.py
@@ -0,0 +1,109 @@
+from typing import Any
+
+import h5py as h5
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import numpy as np
+from numpy.typing import NDArray
+import pandas as pd
+from plotid.publish import publish
+from plotid.tagplot import tagplot
+
+
+def read_metadata(file: str, path: str, attr_key: str) -> Any | None:
+    pass
+
+
+def read_data(file: str, path: str) -> NDArray | None:
+    pass
+
+
+def check_equal_length(*arrays: NDArray) -> bool:
+    pass
+
+
+def process_time_data(data: NDArray) -> NDArray:
+    pass
+
+
+def remove_negatives(array: NDArray) -> NDArray:
+    pass
+
+
+def linear_interpolation(
+    time: NDArray, start_time: float, end_time: float, start_y: float, end_y: float
+) -> NDArray:
+    pass
+
+
+def interpolate_nan_data(time: NDArray, y_data: NDArray) -> NDArray:
+    pass
+
+
+def filter_data(data: NDArray, window_size: int) -> NDArray:
+    """Filter data using a moving average approach.
+
+    Args:
+        data (NDArray): Data to be filtered
+        window_size (int): Window size of the filter
+
+    Returns:
+        NDArray: Filtered data
+    """
+    output = []
+    pad_width = window_size // 2
+    padded_data = np.pad(array=data, pad_width=pad_width, mode="empty")
+    for i in range(pad_width, padded_data.size - pad_width):
+        # Implementieren Sie hier den SMA!
+        sma = []
+        output.append(sma)
+    return np.array(output)
+
+
+
+def calc_heater_heat_flux(P_heater: float, eta_heater: float) -> float:
+    pass
+
+
+def calc_convective_heat_flow(
+    k_tank: float, area_tank: float, t_total: float, t_env: float
+) -> float:
+    pass
+
+
+def calc_mass_flow(
+    level_data: NDArray, tank_footprint: float, density: float
+) -> NDArray:
+    pass
+
+
+def calc_transported_power(
+    mass_flow: float, specific_heat_capacity: float, temperature: float
+) -> float:
+    pass
+
+
+def store_plot_data(
+    data: dict[str, NDArray], file_path: str, group_path: str, metadata: dict[str, Any]
+) -> None:
+    pass
+
+
+def read_plot_data(
+    file_path: str, group_path: str
+) -> tuple[pd.DataFrame, dict[str, Any]]:
+    pass
+
+
+def plot_data(data: pd.DataFrame, formats: dict[str, str]) -> Figure:
+    pass
+
+
+def publish_plot(
+    fig: Figure, source_paths: str | list[str], destination_path: str
+) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pass
