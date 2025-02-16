9d09cfdf30e6dff8242c91b711b40f5423ab835d
gkhariton <grantkhariton@gmail.com>
2025-02-16 16:27:11 +0100

    test 2

--- pa-ws2425/main.py
+++ pa-ws2425/main.py
@@ -1,13 +1,18 @@
 import datetime
 import os
 
-import numpy as np
 
-import project.functions as fn
 
 
 def main():
     pass
+file_path = os.path.join("project", "data", "data_GdD_Datensatz_WS2425.h5")
+brewing = "brewing_001"
+tank_id = "B001"
+
+print("HDF5 File Path:", file_path)
+print("Brewing Process ID:", brewing)
+print("Tank ID:", tank_id)
 
 
 if __name__ == "__main__":
